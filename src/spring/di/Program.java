f9b8d5b37a3af2337ab8c891dd4b646b5b46738d
jihoon.yang <jihoon_yang@ksmartech.com>
2019-12-20 10:49:20 -0800

    spring DI. step 01
    create setting.xml and configure beans

--- src/spring/di/Program.java
+++ src/spring/di/Program.java
@@ -9,19 +9,17 @@ import spring.di.ui.InlineExamConsole;
 public class Program {
 
     public static void main(String[] args) {
+
 		/**
-		 * create 2 modules
-		 * -> need to fix
-		 */
+		 * Exam 객체와 ExamConsole 객체를 생성하고, ExamConsole 객체에 Exam 객체를 주입하도록
+		 * setting.xml의 설정을 통해 스프링에 지시하는 방식으로 아래 코드를 변경한다.
+		 *
 		Exam exam = new NewlecExam();
 		ExamConsole console = new GridExamConsole(); // Dependency Injection
-
-		/**
-		 * Injection
-		 * -> need to fix
-		 */
 		console.setExam(exam);
+		*/
 
+		ExamConsole console = ??;
 		/**
 		 * run
 		 */
