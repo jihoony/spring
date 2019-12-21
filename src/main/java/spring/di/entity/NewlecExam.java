3c3610b9e99617ad495b0ac86641b55aa3a97eae
jihoon.yang <jihoon_yang@ksmartech.com>
2019-12-20 18:30:35 -0800

    spring DI, step 09 annotation config - 18
    NewlecExam is not Component 
    change configuration

--- src/main/java/spring/di/entity/NewlecExam.java
+++ src/main/java/spring/di/entity/NewlecExam.java
@@ -1,9 +1,7 @@
 package spring.di.entity;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-@Component
 public class NewlecExam implements Exam {
 
     @Value("20")
